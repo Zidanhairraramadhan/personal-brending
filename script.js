62593d009778afea03abc07a913d07feb2e0bda8
Zidanhairraramadhan <zidanhairra@gmail.com>
2025-03-14 16:14:18 +0700

    v

---
+++ script.js
@@ -0,0 +1,9 @@
+// Contoh JavaScript untuk animasi scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
